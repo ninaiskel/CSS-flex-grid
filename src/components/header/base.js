02959df90ab99c0cfad5bc327463a79599f8d41e
Jessica Aiskel Aristimuño <jeakel@gmail.com>
2018-01-30 12:42:57 +0100

    Create header component

--- src/components/header/base.js
+++ src/components/header/base.js
@@ -4,10 +4,16 @@ import './header.css';
 class Header extends Component {
   render() {
     return (
-      <div className="header">
-        hola soy el header
+      <section className="header">
+        <div className="logo">CSS-Léx</div>
+        <div className="share-links">
+          <p>Comparte:</p>
+          <a className="share-links-icon"href="#" >twitter</a>
+          <a className="share-links-icon"href="#">fb</a>
+          <a className="share-links-icon"href="#">github</a>
 
-      </div>
+        </div>
+      </section>
     );
   }
 }

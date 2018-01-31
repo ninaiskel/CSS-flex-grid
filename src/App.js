a68a0a8e57d2614329aeb07ef1c58205f1a16d8b
Jessica Aiskel Aristimuño <jeakel@gmail.com>
2018-01-31 11:03:43 +0100

    improvements grid areas/ Add  footer

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import React, { Component } from 'react';
 import Header from './components/header/base.js';
 import Sidebar from './components/sidebar/base.js';
 import Content from './components/content/base.js';
+import Footer from './components/footer/base.js';
 
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App container">
         <Sidebar  />
         <Header className="header" />
         <Content  />
+        <Footer  />
       </div>
     );
   }
---
+++ src/components/footer/base.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './footer.css';
+
+class Footer extends Component {
+  render() {
+    return (
+      <section className="footer">
+        footer
+      </section>
+    );
+  }
+}
+
+export default Footer;

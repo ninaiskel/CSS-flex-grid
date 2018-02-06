bae6c7cc38f9e473d3d065999f4146601e71e07b
Jessica Aiskel Aristimuño <jeakel@gmail.com>
2018-02-06 17:56:12 +0100

    change name to images

--- src/components/content/example-img.js
+++ src/components/content/example-img.js
@@ -4,8 +4,8 @@ import './content.css';
 const ExampleImg = () => (
   <div className="example-img">
     <h2>Example</h2>
-    <img className="img-property img-property-big" src="img/svg/align-items.svg" alt="property"/>
-    <img className="img-property img-property-small" src="img/svg/align-items-mobile.svg" alt="property"/>
+    <img className="img-property img-property-big" src="img/svg/align-content.svg" alt="property"/>
+    <img className="img-property img-property-small" src="img/svg/align-content-mobile.svg" alt="property"/>
   </div>
 );
 

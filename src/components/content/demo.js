c3b495dfad64f94490da940d02c02f8a19d17c64
Jessica Aiskel Aristimuño <jeakel@gmail.com>
2018-03-30 16:18:00 +0200

    Update deploy command and basic integration of `react-codepen-embed`

--- src/components/content/demo.js
+++ src/components/content/demo.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import Codepen from 'react-codepen-embed';
+
 
 const Demo = () => (
   <div className="demo">
     <h2>Demo</h2>
     <div className="demo-pen">
-    <p data-height="354" data-theme-id="0" data-slug-hash="KQvOOL" data-default-tab="html,result" data-user="Ninaiskel" data-embed-version="2" data-pen-title="flexbox: align-content" data-preview="true" class="codepen">See the Pen <a href="https://codepen.io/Ninaiskel/pen/KQvOOL/">flexbox: align-content</a> by Jessica Aiskel (<a href="https://codepen.io/Ninaiskel">@Ninaiskel</a>) on <a href="https://codepen.io">CodePen</a>.</p>
-<script async src="https://production-assets.codepen.io/assets/embed/ei.js"></script>
+      <Codepen
+        hash="qxXgNj"
+        user="Ninaiskel"
+      />
+
     </div>
   </div>
 );
